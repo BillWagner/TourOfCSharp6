d495826e0448cd110d8b8564c52b75656ee027a9
Bill Wagner <bill.w.wagner@outlook.com>
2014-11-13 11:49:32 -0500

    Add a C# style Point class
    This class has none of the C# 6 features yet.

---
+++ TourOfCSharp6/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourOfCSharp6
+{
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Distance
+        {
+            get
+            {
+                return Math.Sqrt(X * X + Y * Y);
+            }
+        }
+    }
+}
