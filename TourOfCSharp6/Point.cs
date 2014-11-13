675d00d1046954421c9c998d82a087f3d8eaccc2
Bill Wagner <bill.w.wagner@outlook.com>
2014-11-13 15:19:18 -0500

    Update for String Interpolation

--- TourOfCSharp6/Point.cs
+++ TourOfCSharp6/Point.cs
@@ -15,7 +15,7 @@ namespace TourOfCSharp6
 
         public override string ToString()
         {
-            return string.Format("{{{0:F2}, {1:F2}}}, Distance = {2:F2}", X, Y, Distance);
+            return string.Format("\{X:F2}, \{Y:F2}, Distance = \{Distance:F2}");
         }
     }
 }
