c5e869178505fff850e677ae414bc3d2ae34a221
Bill Wagner <bill.w.wagner@outlook.com>
2014-11-13 14:47:46 -0500

    Using Static and extension methods

--- TourOfCSharp6/Program.cs
+++ TourOfCSharp6/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Enumerable;
 
 namespace TourOfCSharp6
 {
@@ -11,8 +8,26 @@ namespace TourOfCSharp6
         static void Main(string[] args)
         {
             var random = new System.Random();
-            var sequence = from n in Enumerable.Range(0, 100)
-                           select new Point { X = random.NextDouble() * 1000, Y = random.NextDouble() * 1000 };
+            var range = Range(1, 100);
+            var sequence = from n in range
+                           select new Point
+                           {
+                               X = random.NextDouble() * 1000,
+                               Y = random.NextDouble() * 1000
+                           };
+
+            var sequence2 = range
+                .Select(n => new Point
+                {
+                    X = random.NextDouble() * 1000,
+                    Y = random.NextDouble() * 1000
+                });
+
+            var sequence3 = Select(range, n => new Point
+            {
+                X = random.NextDouble() * 1000,
+                Y = random.NextDouble() * 1000
+            });
 
             foreach (var item in sequence)
                 Console.WriteLine(item);
